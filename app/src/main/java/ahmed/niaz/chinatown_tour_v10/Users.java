8e0fcf645abc43bff34f5a94fc1dabcb51c009b8
WilliamZhong <wz16874n@pace.edu>
2018-04-03 23:07:22 -0400

    Added feedback feature, linked Firebase to record user feedback and emails

--- app/src/main/java/ahmed/niaz/chinatown_tour_v10/Users.java
+++ app/src/main/java/ahmed/niaz/chinatown_tour_v10/Users.java
@@ -5,4 +5,27 @@ package ahmed.niaz.chinatown_tour_v10;
  */
 
 public class Users {
+
+    String userID;
+    String feedback;
+    String email;
+
+    public Users(String userID, String feedback, String email) {
+        this.userID = userID;
+        this.feedback = feedback;
+        this.email = email;
+    }
+
+/*
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public String getEmail() { return email; }
+    */
+
 }
